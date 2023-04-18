d89fe7c1dd8d5d2cd163f67d5f90f50774075ab7
Dilara <dilara.0880@gmail.com>
2023-04-18 10:57:11 +0300

    created views for Home Page

--- board/views.py
+++ board/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from board.models import Board
+
+
+def index(request):
+    context = {
+        "boards": Board.objects.order_by('id'),
+    }
+    return render(request, 'index.html', context)
+
+
+def view_board(request, board_id):
+    return HttpResponse(f"You're looking on board #{board_id}")
+
+
+def create_task(request):
+    return HttpResponse("You're creating a new task")
+
+
+def choose_board(request):
+    return HttpResponse("You're choosing a board")
