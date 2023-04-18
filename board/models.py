11af242083c495f936e372247b40486c0f0b2ad5
Dilara <dilara.0880@gmail.com>
2023-04-18 10:54:36 +0300

    updated Task class

--- board/models.py
+++ board/models.py
@@ -3,11 +3,15 @@ from django.db import models
 
 
 class Task(models.Model):
+    class Priority(models.TextChoices):
+        easy = 'Easy'
+        medium = 'Medium'
+        hard = 'Hard'
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
     creation_date = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField()
-    priority = models.TextChoices(['Easy', 'Medium', 'Hard'])
+    priority = models.CharField(max_length=6, choices=Priority.choices, default=Priority.medium)
 
 
 class Column(models.Model):
