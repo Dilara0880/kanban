a15415a2789f8632a7ba8fdb88a1ab04a554e839
Dilara <dilara.0880@gmail.com>
2023-04-19 10:01:36 +0300

    added a form for creation a new task

--- board/views.py
+++ board/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from board.models import Board, Column, Task
 
@@ -22,12 +22,26 @@ def view_board(request, board_id):
         'columns': columns,
         'tasks': tasks,
     }
-    print(board.title)
     return render(request, 'view_board.html', context)
 
 
-def create_task(request):
-    return HttpResponse("You're creating a new task")
+def create_task(request, board_id):
+    columns = Column.objects.filter(board_id=board_id)
+    priority_choices = Task.Priority.choices
+    context = {'columns': columns,
+               'board_id': board_id,
+               'priority_choices': priority_choices,
+               }
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        column_id = request.POST.get('column')
+        deadline = request.POST.get('deadline')
+        priority = request.POST.get('priority')
+        task = Task(title=title, description=description, column_id=column_id, deadline=deadline, priority=priority)
+        task.save()
+        return redirect('view_board', board_id)
+    return render(request, 'create_new_task.html', context)
 
 
 def choose_board(request):
