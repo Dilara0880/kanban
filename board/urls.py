bd350423679aa24cc2ce36ac8a2c544651dd2104
Dilara <dilara.0880@gmail.com>
2023-04-19 10:02:07 +0300

    updated some stuff

--- board/urls.py
+++ board/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("board/<int:board_id>/", views.view_board, name="view_board"),
     path("board/", views.choose_board, name="choose_board"),
-    path("task/", views.create_task, name="create_task"),
+    path("task/<int:board_id>/", views.create_task, name="create_task"),
 ]
