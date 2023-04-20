68adb3797e10f0f21ad50f0a2d387ebb197eceed
Dilara <dilara.0880@gmail.com>
2023-04-20 09:21:04 +0300

    added view and html for editing a task from board view

--- board/urls.py
+++ board/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("board/<int:board_id>/", views.view_board, name="view_board"),
     path("board/", views.choose_board, name="choose_board"),
     path("task/<int:board_id>/", views.create_task, name="create_task"),
+    path("edittask/<int:board_id>/<int:task_id>/", views.edit_task, name="edit_task"),
 ]
--- board/views.py
+++ board/views.py
@@ -4,6 +4,16 @@ from django.shortcuts import render, get_object_or_404, redirect
 from board.models import Board, Column, Task
 
 
+def update_task_db(request):
+    title = request.POST.get('title')
+    description = request.POST.get('description')
+    column_id = request.POST.get('column')
+    deadline = request.POST.get('deadline')
+    priority = request.POST.get('priority')
+    task = Task(title=title, description=description, column_id=column_id, deadline=deadline, priority=priority)
+    task.save()
+
+
 def index(request):
     context = {
         "boards": Board.objects.order_by('id'),
@@ -33,16 +43,27 @@ def create_task(request, board_id):
                'priority_choices': priority_choices,
                }
     if request.method == 'POST':
-        title = request.POST.get('title')
-        description = request.POST.get('description')
-        column_id = request.POST.get('column')
-        deadline = request.POST.get('deadline')
-        priority = request.POST.get('priority')
-        task = Task(title=title, description=description, column_id=column_id, deadline=deadline, priority=priority)
-        task.save()
+        update_task_db(request)
         return redirect('view_board', board_id)
     return render(request, 'create_new_task.html', context)
 
 
 def choose_board(request):
     return HttpResponse("You're choosing a board")
+
+
+def edit_task(request, board_id, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task_column = get_object_or_404(Column, pk=task.column_id)
+    columns = Column.objects.filter(board_id=board_id)
+    context = {
+        'task': task,
+        'board_id': board_id,
+        'task_id': task_id,
+        'task_column': task_column,
+        'columns': columns,
+    }
+    if request.method == 'POST':
+        update_task_db(request)
+        return redirect('view_board', board_id)
+    return render(request, 'edit_task.html', context)
